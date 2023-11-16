8a3ee3f219c91ca036f8b120b0f83f71e635fbf7
Arnan (Roger) Sipitakiat <arnans@gmail.com>
2023-11-16 18:55:36 +0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+# Import the Canvas class
+from canvasapi import Canvas
+
+# Canvas API URL
+API_URL = "https://mango-cmu.instructure.com"
+# Canvas API key
+API_KEY = "21123~emkSDtMv6THZj0MxDAlcSnKGi7vCbv6Uam0VI1bUerOyC1xo5QL8LAnXEtWD7vpx"
+
+# Initialize a new Canvas object
+canvas = Canvas(API_URL, API_KEY)
